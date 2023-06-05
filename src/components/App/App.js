97d68c9485c007207b8f146e52deb79366ec59fc
Kara Anderson <kanderson8585@gmail.com>
2023-06-05 09:50:31 -0600

    Update everything to functional components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,47 +1,35 @@
 import './App.css';
-import { Component } from 'react';
-// import { Route, Link } from 'react-router-dom';
 import { getFish } from '../../apiCalls';
 import { Collection } from '../Collection/Collection';
 import { Fish } from '../Fish/Fish';
+import { useEffect, useState } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      allFish: [],
-      collectedFish: [],
-      caughtFish: {}
-    }
-    this.fishList = [];
-  }
+const App = () => {
+  const [allFish, setAllFish] = useState([]);
+  const [collectedFish, setCollectedFish] = useState([]);
+  const [fishList, setFishList] = useState([]);
 
-  componentDidMount = () => {
+  useEffect(() => {
     getFish().then(data => {
-      this.setState({ allFish: Object.keys(data).map(key => data[key]) })
+      setAllFish(Object.keys(data).map(key => data[key]))
     })
+  })
+
+  const goFishing = () => {
+    setFishList([...fishList, allFish[Math.floor(Math.random() * 80)]]);
+    setCollectedFish(allFish.filter(fish => fishList.includes(fish)))
   }
 
-  goFishing = () => {
-    this.fishList.push((this.state.allFish[Math.floor(Math.random() * 80)]))
-    this.setState({ 
-      caughtFish: this.fishList[this.fishList.length - 1], 
-      collectedFish: (this.state.allFish.filter(fish => this.fishList.includes(fish))) 
-    })
-  }
-  
-  render() {
-    return (
-      <div>
-        <Fish goFishing={this.goFishing}/>
-        <h1>My Collection</h1>
-        <div className='main'>
-          {this.state.collectedFish.sort((a, b) => a.id - b.id).map(fish => {
-            return <Collection key={fish['file-name']} fish={fish} />})}
-        </div>
+  return (
+    <div>
+      <Fish goFishing={goFishing}/>
+      <h1>My Collection</h1>
+      <div className='main'>
+        {collectedFish.sort((a, b) => a.id - b.id).map(fish => {
+          return <Collection key={fish['file-name']} fish={fish} />})}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
